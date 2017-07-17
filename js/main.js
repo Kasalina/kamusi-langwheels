69e557f54bb1e4bfaab244c60f4549e6144fa176
Haran Kumar <hkumar8200@gmail.com>
2017-07-17 18:04:04 -0500

    Added submit/mailto component

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 const wheelID = "wheel-container"
 const wheel = document.getElementById(wheelID)
-const wheelSize = window.innerHeight * 0.6
-let colors = ["W", "W", "W", "W", "W", "W"]
-let code = ""
-
-const colorsToHex = {
+const WHEEL_SIZE = window.innerHeight * 0.6
+const COLOR_VALUES = {
   "R": "#FF0000",
   "O": "#FF8000",
   "Y": "#FFFF00",
@@ -15,6 +12,15 @@ const colorsToHex = {
   "X": "#000000",
   "W": "#FFFFFF"
 }
+const KAMUSI_EMAIL = "contact+colors@kamusi.org"
+const submit = document.getElementById("submit")
+const rationale = document.getElementById("rationale")
+
+let colors = ["W", "W", "W", "W", "W", "W"]
+let current_wheel = []
+let current_code = ""
+
+
 
 function generateWedgeString(startX, startY, startAngle, endAngle, radius) {
   var x1 = startX - radius * Math.cos(Math.PI * startAngle / 180);
@@ -29,15 +35,16 @@ function generateWedgeString(startX, startY, startAngle, endAngle, radius) {
 }
 
 function updateUI() {
+  updateMailTo()
   wheel.innerHTML = ""
-  var draw = SVG(wheelID).size(wheelSize, wheelSize)
-  var sw = wheelSize / 40.0
+  var draw = SVG(wheelID).size(WHEEL_SIZE, WHEEL_SIZE)
+  var sw = WHEEL_SIZE / 40.0
   for (let i = 0; i < 6; i += 1) {
     var angle = i * 60.0 + 90;
-    var pathStr = generateWedgeString(wheelSize / 2, wheelSize / 2, angle, angle + 60.0, wheelSize / 2 - sw)
+    var pathStr = generateWedgeString(WHEEL_SIZE / 2, WHEEL_SIZE / 2, angle, angle + 60.0, WHEEL_SIZE / 2 - sw)
     var path = draw.path(pathStr)
     path.attr({
-      fill: colorsToHex[colors[i]],
+      fill: COLOR_VALUES[colors[i]],
       stroke: "#000",
       "stroke-width": sw
     })
@@ -47,10 +54,13 @@ function updateUI() {
 }
 
 function loadLang(code) {
+  current_code = code
+
   $.ajax({
     url: "https://kamusi-langwheels-ad6af.firebaseio.com/" + code + ".json",
     success: function(data){
       colors = data.split("")
+      current_wheel = colors
       updateUI()
     },
     error: function(data){
@@ -59,16 +69,33 @@ function loadLang(code) {
   })
 }
 
-for (let el of document.getElementsByClassName("color-option")) {
-
-  el.onclick = function() {
-    for (let e of el.parentElement.children) {
-      e.classList.remove("selected")
-    }
-    el.classList.add("selected")
-    colors[el.parentElement.getAttribute("data-target")] = el.getAttribute("data-color")
-    updateUI()
-  }
+function updateMailTo(){
+  const subject = "Color wheel proposal for " + current_code
+  const body = "CURRENT WHEEL: " + current_wheel.join("") + "\n"
+             + "NEW WHEEL: " + colors.join("")
+             + "RATIONALE: " + rationale.value
+  submit.href = generateMailToString(subject, body)
 }
 
-updateUI()
+function generateMailToString(subject, body){
+  return encodeURIComponent("mailto:" + KAMUSI_EMAIL +
+  "?subject=" + subject +
+  "&body=" + body)
+}
+
+$(document).ready(function(){
+  for (let el of document.getElementsByClassName("color-option")) {
+    el.onclick = function() {
+      for (let e of el.parentElement.children) {
+        e.classList.remove("selected")
+      }
+      el.classList.add("selected")
+      colors[el.parentElement.getAttribute("data-target")] = el.getAttribute("data-color")
+      updateUI()
+    }
+  }
+
+  $(rationale).change(updateMailTo)
+
+  updateUI()
+})
