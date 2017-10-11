339d18524b6da708412d82f3cb66e7f42e293758
Haran Kumar <hkumar8200@gmail.com>
2017-10-11 10:36:58 -0500

    Update langselect.js

--- js/viewer/langselect.js
+++ js/viewer/langselect.js
@@ -1,112 +1,87 @@
 var rtlLangs = new Set([
-  "heb",
-  "fas",
-  "mzn",
-  "lrc",
-  "uig",
-  "kas",
-  "urd",
-  "fa_AF",
-  "ara",
-  "yid",
-  "ckb",
-  "pus",
-  "ur_IN",
-  "ar_EG",
-  "ar_LY",
-  "ar_SA",
-  "uz_Arab",
-  "pa_Arab"
+    "heb",
+    "fas",
+    "mzn",
+    "lrc",
+    "uig",
+    "kas",
+    "urd",
+    "fa_AF",
+    "ara",
+    "yid",
+    "ckb",
+    "pus",
+    "ur_IN",
+    "ar_EG",
+    "ar_LY",
+    "ar_SA",
+    "uz_Arab",
+    "pa_Arab"
 ])
 
 const userlangAJAX = {
-  url: function(params) {
-    return 'https://kamusi-cls-backend.herokuapp.com/userlangs/' + (params.term || "")
-  },
-  dataType: 'json',
-  delay: 100,
-  processResults: function(data) {
-    var ret = data
-    if (data[0] && typeof data[0].text !== "string") {
-      ret = []
-      for (var i = 0; i < data.length; i++) {
-        for (var j = 0; j < data[i].text.length; j++) {
-          ret.push({
-            text: data[i].text[j],
-            id: data[i].id
-          })
+    url: function (params) {
+        return 'https://kamusi-cls-backend.herokuapp.com/userlangs/' + (params.term || "")
+    },
+    dataType: 'json',
+    delay: 100,
+    processResults: function (data) {
+        var ret = data
+        if (data[0] && typeof data[0].text !== "string") {
+            ret = []
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < data[i].text.length; j++) {
+                    ret.push({
+                        text: data[i].text[j],
+                        id: data[i].id
+                    })
+                }
+            }
         }
-      }
-    }
-    console.log(ret)
-    return {
-      results: ret
-    }
-  },
-  minimumInputLength: 3
+        console.log(ret)
+        return {
+            results: ret
+        }
+    },
+    minimumInputLength: 3
 }
 
 function ajaxInit(val, code) {
-  val = val || ""
-  $.ajax("https://kamusi-cls-backend.herokuapp.com/userlangs/" + val.toLowerCase(), {
-    dataType: "json",
-    success: function(data) {
-      var ret = data.slice()
-      ret.unshift({id: code, text: val})
-      if (data[0] && typeof data[0].text !== "string") {
-        ret = []
-        for (var i = 0; i < data.length; i++) {
-          for (var j = 0; j < data[i].text.length; j++) {
-            ret.push({
-              text: data[i].text[j],
-              id: data[i].id
-            })
-          }
-        }
-      }
-      userlangSelector.select2({
-        data: ret,
-        ajax: userlangAJAX,
-        dir: rtlLangs.has(ret[0].id) ? "rtl" : "ltr",
-        width: 500
-      })
-      userlangSelector.trigger('change')
+    if (code === null)
+        return
 
-      userlangSelector.change(function(){
-        userlangSelector.select2(
-          {dir: rtlLangs.has(userlangSelector.val()) ? "rtl" : "ltr", width: 500}
-        )
-      })
-    },
-    error: function(err) {
-      throw err
-    }
-  })
+    if (code === val)
+        codeDisp.text(code)
+    else
+        codeDisp.text(code + " (" + val + ")")
+
+    loadLang(code)
 }
 
 // avoid duplicate selectors
 var userlangSelector, codeDisp
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  // initialize userlang selector
-  userlangSelector = $("#userlang")
-  codeDisp = $("#code")
+    // initialize userlang selector
+    userlangSelector = $("#userlang")
+    codeDisp = $("#code")
 
-  userlangSelector.select2({
-    ajax: userlangAJAX,
-    width: 500
-  })
+    userlangSelector.select2({
+        ajax: userlangAJAX,
+        width: 500,
+        placeholder: "Select a language"
+    })
 
-  userlangSelector.change(function(e) {
-    codeDisp.text(userlangSelector.val())
-    loadLang(userlangSelector.val())
-  })
+    userlangSelector.change(function (e) {
+        codeDisp.text(userlangSelector.val())
+        loadLang(userlangSelector.val())
+    })
 
-  const params = getQueryParams(document.location.search)
-  if (params["lang"] && params["code"])
-    ajaxInit(params["lang"], params["code"])
-  else {
-    ajaxInit()
-  }
+    const params = getQueryParams(document.location.search)
+    if (params["lang"] && params["code"])
+        ajaxInit(params["lang"], params["code"])
+    else {
+        ajaxInit("No Language Selected", null)
+    }
 })
